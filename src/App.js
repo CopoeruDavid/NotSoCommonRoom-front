edf31c07e5914eb7bbd6bdd55fd0b38586a3a41f
K1889468 <k1889468@kcl.ac.uk>
2020-12-05 18:13:28 +0000

    Trying the chat

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navbar from './components/Navbar';
 import LogIn from './components/LogIn';
 import{BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import Scheduler from './components/scheduler.js';
+import Chat from './components/chat.js';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
            <Route path ="/events" component={Scheduler} />
           <Route path='/' exact component={Home} />
           <Route path='/sign-up' exact component={LogIn} />
+          <Route path='/chat' component={Chat}/>
         </Switch>
       </Router>
     </div>
