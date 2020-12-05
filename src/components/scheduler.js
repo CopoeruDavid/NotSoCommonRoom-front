89c88d4a7fd5ccf94fff484f08cbd9d0903c7f41
K1889468 <k1889468@kcl.ac.uk>
2020-12-05 16:49:45 +0000

    Month scheduler

--- src/components/scheduler.js
+++ src/components/scheduler.js
@@ -1,11 +1,18 @@
 import React, {Component} from 'react';
-import {DayPilotScheduler} from "daypilot-pro-react";
+import {DayPilot, DayPilotScheduler} from "daypilot-pro-react";
 
 class Scheduler extends Component {
     render() {
         return (
-                <DayPilotScheduler
-                    resources = {[
+            <DayPilotScheduler
+                startDate = {DayPilot.Date.today().firstDayOfMonth()}
+                days = {DayPilot.Date.today().daysInMonth()}
+                scale = {"Day"}
+                timeHeaders = {[
+                    { groupBy: "Month"},
+                    { groupBy: "Day", format: "d"}
+                ]}
+                resources = {[
                     {name: "Resource A", id: "A"},
                     {name: "Resource B", id: "B"},
                     {name: "Resource C", id: "C"},
